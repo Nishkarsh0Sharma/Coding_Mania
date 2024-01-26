21ab10480ab1c3453c46055040f92ec0a9701978
Nishkarsh Sharma <nishkarshsharma2004@gmail.com>
2024-01-26 12:34:07 +0530

    LL and poly reprenstation

---
+++ Linked_List/Display_LL.c
@@ -0,0 +1,121 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node 
+{
+    int data;
+    struct Node *next;
+}
+
+*first=NULL;
+
+void create(int A[],int n)
+{
+    int i;
+    struct Node *t,*last;
+    first=(struct Node *)malloc(sizeof(struct Node));
+    first->data=A[0];
+    first->next=NULL;
+    last=first;
+
+    for(i=1;i<n;i++)
+    {
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+}
+
+int count(struct Node *p)
+{
+    int l=0;
+    while(p)
+    {
+        l++;
+        p=p->next;
+    }
+    return l;
+}
+
+int Rcount(struct Node *p)
+{
+    if(p!=NULL)
+    {
+        return Rcount(p->next)+1;
+    } return 0;
+}
+
+int sum(struct Node *p)
+{
+    int s=0;
+    while(p!=NULL)
+    {
+        s+=p->data;
+        p=p->next;
+    } 
+    return s;
+}
+
+int Rsum(struct Node *p)
+{
+    if(p==NULL)
+     return 0;
+    else 
+     return Rsum(p->next)+p->data;
+}
+
+int Max(struct Node *p)
+{
+    int Max=INT_MIN;
+    while(p)
+    {
+        if(p->data>Max)
+         Max=p->data;
+        p=p->next;
+    } 
+    return Max;
+}
+
+int RMax(struct Node *p)
+{
+    int x=0;
+    if(p==0)
+     return INT_MIN;
+    x=RMax(p->next);
+    if(x>p->data)
+     return x;
+    else 
+     return p->data;
+}
+
+void display(struct Node *p)
+{
+    while(p!=NULL)
+    {
+        printf("%d ",p->data);
+        p=p->next;
+    }
+}
+
+void Rdisplay(struct Node *p)
+{
+    if(p!=NULL)
+    {
+        Rdisplay(p->next);
+        printf("%d ",p->data);
+    }
+}
+
+int main()
+{
+    int A[]={3,5,10,8,4,12,7,2};
+    struct Node *temp;
+    
+    create(A,8);
+    RMax(first);
+    display(first);
+
+    return 0;
+}
