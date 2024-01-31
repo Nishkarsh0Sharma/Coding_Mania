aa6e5084c9438cc6fb1587f679416f9f92865edd
Nishkarsh Sharma <nishkarshsharma2004@gmail.com>
2024-01-31 18:31:14 +0530

    modified RPS game in PY!

--- python/RPS_game.py
+++ python/RPS_game.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -26,21 +27,30 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
-import random 
+game_images =[rock,paper,scissors] 
 user_input=int(input("What do you choose ? Type 0 for Rock, 1 for paper or 2 for scissors ."))
-Computer_chose=random.randint(0,2)
-print(f"Computer chose {Computer_chose}")
 
-if user_input==0 and Computer_chose==2:
-  print("You win")
-elif Computer_chose > user_input :
-  print("Computer wins")
-elif Computer_chose == user_input :
-  print("It's a draw")
-elif user_input > Computer_chose:
-  print("You wins")
-else:
+if user_input >= 3 or user_input < 0:
   print("You typed an invalid number, you lose!")
+else:
+  print(game_images[user_input])
+  
+  Computer_chose=random.randint(0,2)
+  print(f"Computer chose {Computer_chose}")
+  print(game_images[Computer_chose])
+  
+  
+  if user_input == 0 and Computer_chose == 2 :
+    print("You win!")
+  elif user_input == 2  and Computer_chose == 0:
+    print("You loose")
+  elif Computer_chose > user_input:
+    print("You losse")
+  elif user_input > Computer_chose :
+    print("You win!")
+  elif Computer_chose == user_input:
+    print("It's draw")
+  
   
   
   
