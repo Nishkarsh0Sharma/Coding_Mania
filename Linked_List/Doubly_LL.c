c3b0fc69b11936e152d076a95fa44d5e20473223
Nishkarsh Sharma <nishkarshsharma2004@gmail.com>
2024-02-01 13:28:53 +0530

    Inserting in a Doubly LL
    Inserting in a Doubly LL:
    1.Before first Node.
    2.At any position
    :)

--- Linked_List/Doubly_LL.c
+++ Linked_List/Doubly_LL.c
@@ -29,6 +29,7 @@ void create(int A[],int n)
     }
 }
 
+
 void Display(struct Node *p)
 {
     p=first;
@@ -52,12 +53,51 @@ int lenght(struct Node *p)
     return len;
 }
 
+//Inserting in a Doubly LL
+//1.Before first Node.
+//2.At any position
+void insert(struct Node *p,int index ,int x)
+{
+    struct Node *t;
+    if( index < 0 || index > lenght(first))
+     return ;
+    t=(struct Node *)malloc(sizeof(struct Node));
+    if(index==0)
+    {
+        t->data=x;
+        t->pre=NULL;
+        if(first==NULL)
+        {
+            first->next=NULL;
+            first=t;
+        }
+        else
+        {
+            t->next=first;
+            first->pre=t;
+            first=t;
+        }
+    }
+    else 
+    {
+        for(int i=0;i<index-1;i++)p=p->next;
+        t->data=x;
+        t->next=p->next;
+        t->pre=p;
+        if(p->next)p->next->pre=t;
+        p->next=t;
+    }
+}
+
+
 
 int main()
 {
     int A[]={8,2,3,9,4,1};
     create(A,6);
     printf("number of element is: %d \n",lenght(first));
+    insert(first,0,11);
+    insert(first,7,99);
     Display(first);
     return 0;
 }
