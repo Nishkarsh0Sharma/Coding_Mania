60bd3f435a279faee4dd3a98165edfab2d0698ff
Nishkarsh Sharma <nishkarshsharma2004@gmail.com>
2024-02-01 12:59:20 +0530

    Display Doubly LL
    simply creating Doubly LL and Display it.
    :)

---
+++ Linked_List/Doubly_LL.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    struct Node *pre;
+    int data;
+    struct Node *next;
+}
+*first=NULL;
+
+void create(int A[],int n)
+{
+    struct Node *t,*last;
+    int i;
+    first=(struct Node *)malloc(sizeof(struct Node));
+    first->data=A[0];
+    first->next=first->pre=NULL;
+    last=first;
+
+    for(i=1;i<n;i++)
+    {
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=A[i];
+        t->next=last->next;
+        t->pre=last;
+        last->next=t;
+        last=t;
+    }
+}
+
+void Display(struct Node *p)
+{
+    p=first;
+    while(p!=NULL)
+    {
+        printf("%d ",p->data);
+        p=p->next;
+    }
+    printf("\n");
+}
+
+
+
+int main()
+{
+    int A[]={8,2,3,9,4,1};
+    create(A,6);
+    // printf("number of element is: %d \n",lenght(first));
+    Display(first);
+    return 0;
+}
