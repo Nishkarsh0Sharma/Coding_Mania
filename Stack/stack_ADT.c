1c2f559c234f60db8d50f99fd34c05764023ae8a
Nishkarsh Sharma <nishkarshsharma2004@gmail.com>
2024-02-03 10:37:05 +0530

    peaking in the stack
    :)

--- Stack/stack_ADT.c
+++ Stack/stack_ADT.c
@@ -54,19 +54,32 @@ int pop(struct stack *st)
     return x;
 }
 
-
+int peak(struct stack st,int pos)
+{
+    int x=-1;
+    if(st.Top-pos+1<0)
+        printf("Invalid position");
+    else
+    {
+        x=st.S[st.Top-pos+1];
+    }
+    return x;
+}
 
 int main()
 {
     struct stack st;
     create(&st);
     push(&st,10);
-    // push(&st,20);
-    // push(&st,30);
-    // push(&st,40);
-    // push(&st,50);
-    printf("%d\n",pop(&st));
-    printf("%d\n",pop(&st));
+    push(&st,20);
+    push(&st,30);
+    push(&st,40);
+    push(&st,50);
+
+    // printf("poping value is : %d\n",pop(&st));
+    // printf("poping value is : %d\n",pop(&st));
+
+    printf("peaking value is : %d\n",peak(st,3));
     Display(st);
     return 0;
 };
