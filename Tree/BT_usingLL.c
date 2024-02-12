2541b61a31809b707244d857626c6cad6924b2cc
Nishkarsh Sharma <nishkarshsharma2004@gmail.com>
2024-02-12 23:52:30 +0530

    draft stage 1 for creating Binary Tree
    Header file 'Queue.h' are under process

--- Tree/BT_usingLL.c
+++ Tree/BT_usingLL.c
@@ -1,50 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-// #include "Queue.h"
-
-
-struct Queue
-{
-    int size;
-    int front;
-    int Rear;
-    int **Q;    //Node type pointer to a pointer
-};
-
-void create(struct Queue *q,int size)
-{
-    q->size=size;
-    q->front=q->Rear=0;
-    q->Q=(int **)malloc(q->size*sizeof(int *));
-}
-
-void enqueue(struct Queue *q,struct Node *x)
-{
-    if((q->Rear+1)%q->size==q->front)
-        printf("Queue is full\n");
-    else
-    {
-        q->Rear=(q->Rear+1)%q->size;
-        q->Q[q->Rear]=x;
-    }
-}
-
-struct Node * dequeue(struct Queue *q)
-{
-    struct Node *x=NULL;
-    if(q->front==q->Rear)
-        printf("Queue is Empty\n");
-    else
-    {
-        q->front=(q->front+1)%q->size;
-        x=q->Q[q->front];
-    }
-    return x;
-}
+#include "Queue.h"
 
 //////////////////////////////////
 
 struct Node *root=NULL;
+
 void TreeCreate()
 {
     struct Node *t,*p;
