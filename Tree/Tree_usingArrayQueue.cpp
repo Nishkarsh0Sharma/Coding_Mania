4c83b850454606d94d0f0a798c1326e237a0ff14
Nishkarsh Sharma <nishkarshsharma2004@gmail.com>
2024-02-20 23:15:42 +0530

    processing on Tree using Array Queue
    :)

---
+++ Tree/Tree_usingArrayQueue.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+public:
+    Node *lchild;
+    int data;
+    Node *rchild;
+};
+
+class Queue
+{
+private:
+    int size;
+    int front;
+    int rear;
+    Node **Q;   //[Node *]* : pointer to [pointer to node]
+
+public:
+    Queue(int size);
+    ~Queue();
+    bool isFull();
+    bool isEmpty();
+    void enqueue(Node *x);
+    Node *dequeue();
+
+};
+
+
+Queue::Queue(int size)
+{
+    this->size=size;
+    front=-1;
+    rear=-1;
+    Q=new Node*[size];
+}
+
+Queue::~Queue()
+{
+    delete []Q;
+}
+
+bool Queue::isEmpty()
+{
+    if(front==rear) return true;
+    return false;
+}
+
+bool Queue::isFull()
+{
+    if(rear==size-1) return true;
+    return false;
+}
+
+void Queue::enqueue(Node *x)
+{
+    if (isFull()) cout<<"Queue Overflow"<<endl;
+    else 
+        rear++;
+        Q[rear]=x;
+}
+
+Node *Queue::dequeue()
+{
+    Node* x=nullptr;
+    if(isEmpty())
+    {
+        cout<<"Queue is Underflow"<<endl;
+    }
+    else 
+    {
+        front++;
+        x=Q[front];
+    }
+    return x;
+}
+
+class Tree
+{
+private:
+    Node *root;
+public:
+    Tree(){root=nullptr;}
+    ~Tree();
+    void createTree();
+    void preOrder(){preOrder(root);}    // Passing Private Parameter in Constructor
+    void preOrder(Node *p);
+};
